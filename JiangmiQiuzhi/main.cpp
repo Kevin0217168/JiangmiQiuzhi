50d254fe9b155066d29dd3aeb1a1404cb601a95c
KaiWen <15838189794@163.com>
2020-03-02 11:51:13 +0800

    增加倍数测试、相乘、整数转换函数，未Debug

--- JiangmiQiuzhi/main.cpp
+++ JiangmiQiuzhi/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//#include <stdilb.h>
 
 using namespace std;
 
-struct Exp 
+struct Exp
 {
 	int xishu;
 	int cishu;
@@ -19,7 +18,7 @@ vector<Exp>* fenXi(string exp)
 	vector<Exp>* exp_list = new vector<Exp>;
 	string last_exp;
 	// 循环遍历字符串
-	for (int i = 0; i < exp.size(); i++) 
+	for (int i = 0; i < exp.size(); i++)
 	{
 		// 判断下一个是否为下一个单项式的开始或多项式的末尾
 		if (exp[i] == '+' || exp[i] == '-' || i == exp.size() - 1)
@@ -56,9 +55,11 @@ vector<Exp>* fenXi(string exp)
 						// 判断前系数是否省略，如省略则自动设1
 						if (s == "" || s == "+") {
 							a.xishu = 1;
-						}else if (s == "-") {
+						}
+						else if (s == "-") {
 							a.xishu = -1;
-						}else {
+						}
+						else {
 							a.xishu = atoi(s.data());
 						}
 						break;
@@ -86,7 +87,7 @@ vector<Exp>* fenXi(string exp)
 // 删除空格
 string replace_space(string* exp) {
 	string str;
-	for (int index = 0; index < exp->size(); index++) 
+	for (int index = 0; index < exp->size(); index++)
 	{
 		if ((*exp)[index] != ' ') {
 			str += (*exp)[index];
@@ -96,10 +97,10 @@ string replace_space(string* exp) {
 }
 
 // 补齐次数项
-void if_add_cishu(vector<Exp>* exp) 
+void if_add_cishu(vector<Exp>* exp)
 {
 	int last_cishu = (*exp)[0].cishu + 1;
-	for (vector<Exp>::iterator it = exp->begin(); it < exp->end(); ++it) 
+	for (vector<Exp>::iterator it = exp->begin(); it < exp->end(); ++it)
 	{
 		if (it->cishu != last_cishu - 1)
 		{
@@ -111,15 +112,15 @@ void if_add_cishu(vector<Exp>* exp)
 }
 
 // 删除首位出现的零
-void remove_zero(vector<Exp>* exp) 
+void remove_zero(vector<Exp>* exp)
 {
-	for (vector<Exp>::iterator it = exp->begin(); it < exp->end(); ++it) 
+	for (vector<Exp>::iterator it = exp->begin(); it < exp->end(); ++it)
 	{
-		if (it->xishu == 0) 
+		if (it->xishu == 0)
 		{
 			it = exp->erase(it);
 		}
-		else 
+		else
 		{
 			break;
 		}
@@ -127,14 +128,14 @@ void remove_zero(vector<Exp>* exp)
 }
 
 // 判断被除数是否大于除数
-bool if_dividend_bigger_than_divisor(vector<Exp>* exp, vector<int>* main_exp) 
+bool if_dividend_bigger_than_divisor(vector<Exp>* exp, vector<int>* main_exp)
 {
 	// 判断位数
-	if (exp->size() > main_exp->size()) 
+	if (exp->size() > main_exp->size())
 	{
 		return false;
 	}
-	else if (exp->size() > main_exp->size()) 
+	else if (exp->size() > main_exp->size())
 	{
 		return true;
 	}
@@ -149,28 +150,99 @@ bool if_dividend_bigger_than_divisor(vector<Exp>* exp, vector<int>* main_exp)
 	return true;
 }
 
-// 大除法！！！
-int big_divide(vector<Exp>* exp, vector<Exp>* main_exp) 
+vector<int>* cheng(vector<Exp>* exp, int bei) 
 {
+	vector<int>* result = new vector<int>;
+	for (vector<Exp>::iterator it = exp->begin(); it < exp->end(); ++it) 
+	{
+		result->push_back((it->xishu) * bei);
+	}
+	return result;
+}
+
+int beishu(vector<Exp>* exp, vector<int>* main_exp) 
+{
+	int bei = 2;
+	while (true)
+	{
+		vector<int>* result = cheng(exp, bei);
+		if (if_dividend_bigger_than_divisor(exp, main_exp)) 
+		{
+			bei += 1;
+			delete result;
+		}
+		else {
+			delete result;
+			return bei - 1;
+		}
+	}
+}
+
+// 数组转换整数
+int zhuanhuan(vector<int>* exp) 
+{
+	string str;
+	vector<int> daijian;
+	for (int index = 0; index < exp->size(); index++)
+	{
+		if ((*exp)[index] < 0) 
+		{
+			str += "0";
+			string shu = to_string((*exp)[index]);
+			// 截掉负号
+			shu = shu.substr(1, shu.size());
+			// 该数后补充0
+			shu = shu + string("0", (exp->size() - index + 1));
+			// 转换为整数，添加到待减列表
+			daijian.push_back(atoi(shu.data()));
+		}
+		else 
+		{
+			str += to_string((*exp)[index]);
+		}
+	}
+	int result = atoi(str.data());
+	for (vector<int>::iterator it = daijian.begin(); it < daijian.end(); it++) 
+	{
+		result -= *it;
+	}
+	return result;
+}
+
+// 被除数的每一项减去除数的每一项
+vector<int>* sub(vector<Exp>* exp, vector<int>* main_exp) 
+{
+}
+
+
+// 大除法！！！
+int big_divide(vector<Exp>* exp, vector<Exp>* main_exp)
+{
+	// 删除首位出现的0
+	remove_zero(exp);
+	remove_zero(main_exp);
 	// 从总被除数中取出与除数相同个数的数，添加到当前被除数
 	int beichuNextCount = exp->size();
 	vector<int> dangqian(exp->size());
-	for (int i = 0; i < dangqian.size(); i++) 
-	{
+	for (int i = 0; i < dangqian.size(); i++) {
 		dangqian[i] = (*exp)[i].xishu;
 	}
-	while (true){
+	int bei;
+	while (true) 
+	{
 		// 判断被除数是否大于除数
-		if (if_dividend_bigger_than_divisor(exp, &dangqian))
+		if (if_dividend_bigger_than_divisor(exp, &dangqian)) 
 		{
-			// 遍历测试被除数是除数的多少倍(取整)
+			// 遍历测试被除数是除数的多少倍(取整)(也就是商)
+			bei = beishu(exp, &dangqian);
 
 			// 除数的每一项乘以倍数
+			vector<int>* result = cheng(exp, bei);
 
 			// 被除数的每一项减去除数的每一项
 
-			// 将他们的差设为新一轮的被除数
 
+			// 将他们的差设为新一轮的被除数
 		}
 		else
 		{
@@ -188,13 +260,10 @@ int big_divide(vector<Exp>* exp, vector<Exp>* main_exp)
 		}
 	}
 
-
-
-
 	return 0;
 }
 
-int main(void) 
+int main(void)
 {
 	string exp, main_exp;
 	cout << "请输入已知式(xx xx = 0,)‘=0’省略：";
